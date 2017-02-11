779c3a413d557749d1c1bda63e151768dd60de3f
frankw <fwessels@xs4all.nl>
2017-02-10 16:34:33 -0800

    Prototypes for functions difference, drawing, integral, shifting and svm

---
+++ integral.go
@@ -0,0 +1,4 @@
+package go_cv
+
+// ingroup integral
+//SimdIntegral(const uint8_t * src, size_t srcStride, size_t width, size_t height, uint8_t * sum, size_t sumStride, uint8_t * sqsum, size_t sqsumStride, uint8_t * tilted, size_t tiltedStride, SimdPixelFormatType sumFormat, SimdPixelFormatType sqsumFormat)
---
+++ shifting.go
@@ -0,0 +1,4 @@
+package main
+
+// ingroup shifting
+//SimdShiftBilinear(const uint8_t * src, size_t srcStride, size_t width, size_t height, size_t channelCount, const uint8_t * bkg, size_t bkgStride, const double * shiftX, const double * shiftY, size_t cropLeft, size_t cropTop, size_t cropRight, size_t cropBottom, uint8_t * dst, size_t dstStride)
