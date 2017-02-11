e67ce8d6978503d11bf3f4ea36d95c6c904e62bb
frankw <fwessels@xs4all.nl>
2017-02-10 18:06:51 -0800

    Move AbsDifferenceSum

--- correlation.go
+++ correlation.go
@@ -1,7 +1,30 @@
 package main
 
-// ingroup correlation
-//SimdAbsDifferenceSum(const uint8_t * a, size_t aStride, const uint8_t * b, size_t bStride, size_t width, size_t height, uint64_t * sum)
+// #cgo pkg-config: simd
+// #include "stdlib.h"
+// #include "Simd/SimdLib.h"
+// #cgo LDFLAGS: -lstdc++
+import "C"
+import "unsafe"
+
+// AbsDifferenceSum gets sum of absolute difference of two gray 8-bit images.
+// Both images must have the same width and height.
+// [in] a - first image.
+// [in] b - second image.
+// [out] sum - the result sum of absolute difference of two images.
+func AbsDifferenceSum(a, b View) int64 {
+	ap := unsafe.Pointer(C.CBytes("aaaa"))
+	defer C.free(ap)
+	aStride := 4
+	bp := unsafe.Pointer(C.CBytes("cccc"))
+	defer C.free(bp)
+	bStride := 4
+	width, height := 4, 1
+	sum := C.uint64_t(0)
+	C.SimdAbsDifferenceSum((*C.uint8_t)(ap), C.size_t(aStride), (*C.uint8_t)(bp), C.size_t(bStride), C.size_t(width), C.size_t(height), &sum)
+	return int64(sum)
+}
+
 //SimdAbsDifferenceSumMasked(const uint8_t * a, size_t aStride, const uint8_t * b, size_t bStride, const uint8_t * mask, size_t maskStride, uint8_t index, size_t width, size_t height, uint64_t * sum)
 //SimdAbsDifferenceSums3x3(const uint8_t * current, size_t currentStride, const uint8_t * background, size_t backgroundStride, size_t width, size_t height, uint64_t * sums)
 //SimdAbsDifferenceSums3x3Masked(const uint8_t *current, size_t currentStride, const uint8_t *background, size_t backgroundStride, const uint8_t *mask, size_t maskStride, uint8_t index, size_t width, size_t height, uint64_t * sums)
--- go-cv.go
+++ go-cv.go
@@ -7,28 +7,14 @@ package main
 import "C"
 
 import "fmt"
-import "unsafe"
 
 func Version() string {
 	return C.GoString(C.SimdVersion())
 }
 
-func SimdAbsDifferenceSum() int64 {
-    a := unsafe.Pointer(C.CBytes("aaaa"))
-    defer C.free(a)
-    aStride := 4
-    b := unsafe.Pointer(C.CBytes("cccc"))
-    defer C.free(b)
-    bStride := 4
-    width, height := 4, 1
-    sum := C.uint64_t(0)
-    C.SimdAbsDifferenceSum((*C.uint8_t)(a), C.size_t(aStride), (*C.uint8_t)(b), C.size_t(bStride), C.size_t(width), C.size_t(height), &sum)
-    return int64(sum)
-}
-
 func main() {
 	fmt.Println("Simd version:", Version())
 	fmt.Println("Alignment   :", Alignment())
-    fmt.Println("Crc32c      :", Crc32c("aap"))
-    fmt.Println("AbsDiffSum  :", SimdAbsDifferenceSum())
+	fmt.Println("Crc32c      :", Crc32c("aap"))
+	fmt.Println("AbsDiffSum  :", AbsDifferenceSum(View{}, View{}))
 }
