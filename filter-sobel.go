bef6857efbd2775f1cfbbaac065b06a024bb44a3
frankw <fwessels@xs4all.nl>
2017-02-13 10:09:35 -0800

    Sobel filter

--- filter-sobel.go
+++ filter-sobel.go
@@ -1,9 +1,37 @@
 package main
 
+// #cgo pkg-config: simd
+// #include "stdlib.h"
+// #include "Simd/SimdLib.h"
+// #cgo LDFLAGS: -lstdc++
+import "C"
+
 // ingroup sobel_filter
-//SimdSobelDx(const uint8_t * src, size_t srcStride, size_t width, size_t height, uint8_t * dst, size_t dstStride)
-//SimdSobelDxAbs(const uint8_t * src, size_t srcStride, size_t width, size_t height, uint8_t * dst, size_t dstStride)
-//SimdSobelDxAbsSum(const uint8_t * src, size_t stride, size_t width, size_t height, uint64_t * sum)
-//SimdSobelDy(const uint8_t * src, size_t srcStride, size_t width, size_t height, uint8_t * dst, size_t dstStride)
-//SimdSobelDyAbs(const uint8_t * src, size_t srcStride, size_t width, size_t height, uint8_t * dst, size_t dstStride)
-//SimdSobelDyAbsSum(const uint8_t * src, size_t stride, size_t width, size_t height, uint64_t * sum)
+
+func SobelDx(src, dst View) {
+	C.SimdSobelDx((*C.uint8_t)(src.data), C.size_t(src.stride), C.size_t(src.width), C.size_t(src.height), (*C.uint8_t)(dst.data), C.size_t(dst.stride))
+}
+
+func SobelDxAbs(src, dst View) {
+	C.SimdSobelDxAbs((*C.uint8_t)(src.data), C.size_t(src.stride), C.size_t(src.width), C.size_t(src.height), (*C.uint8_t)(dst.data), C.size_t(dst.stride))
+}
+
+func SobelDxAbsSum(src View) uint64 {
+	sum := C.uint64_t(0)
+	C.SimdSobelDxAbsSum((*C.uint8_t)(src.data), C.size_t(src.stride), C.size_t(src.width), C.size_t(src.height), (*C.uint64_t)(&sum))
+	return uint64(sum)
+}
+
+func SobelDy(src, dst View) {
+	C.SimdSobelDy((*C.uint8_t)(src.data), C.size_t(src.stride), C.size_t(src.width), C.size_t(src.height), (*C.uint8_t)(dst.data), C.size_t(dst.stride))
+}
+
+func SobelDyAbs(src, dst View) {
+	C.SimdSobelDyAbs((*C.uint8_t)(src.data), C.size_t(src.stride), C.size_t(src.width), C.size_t(src.height), (*C.uint8_t)(dst.data), C.size_t(dst.stride))
+}
+
+func SobelDyAbsSum(src View) uint64 {
+	sum := C.uint64_t(0)
+	C.SimdSobelDyAbsSum((*C.uint8_t)(src.data), C.size_t(src.stride), C.size_t(src.width), C.size_t(src.height), (*C.uint64_t)(&sum))
+	return uint64(sum)
+}
