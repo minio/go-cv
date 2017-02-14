46003eab357b94fa45077ae6b174294a8d063d31
frankw <fwessels@xs4all.nl>
2017-02-14 12:33:40 -0800

    Merged all statistics functions into single file

--- statistic-col.go
+++
@@ -1,5 +0,0 @@
-package main
-
-// ingroup col_statistic
-//SimdGetColSums(const uint8_t * src, size_t stride, size_t width, size_t height, uint32_t * sums)
-//SimdGetAbsDxColSums(const uint8_t * src, size_t stride, size_t width, size_t height, uint32_t * sums)
--- statistic.go
+++ statistic.go
@@ -1,5 +1,9 @@
 package main
 
+// ingroup col_statistic
+//SimdGetColSums(const uint8_t * src, size_t stride, size_t width, size_t height, uint32_t * sums)
+//SimdGetAbsDxColSums(const uint8_t * src, size_t stride, size_t width, size_t height, uint32_t * sums)
+
 // ingroup other_statistic
 //SimdGetStatistic(const uint8_t * src, size_t stride, size_t width, size_t height, uint8_t * min, uint8_t * max, uint8_t * average)
 //SimdGetMoments(const uint8_t * mask, size_t stride, size_t width, size_t height, uint8_t index, uint64_t * area, uint64_t * x, uint64_t * y, uint64_t * xx, uint64_t * xy, uint64_t * yy)
@@ -7,3 +11,7 @@ package main
 //SimdSquareSum(const uint8_t * src, size_t stride, size_t width, size_t height, uint64_t * sum)
 //SimdCorrelationSum(const uint8_t * a, size_t aStride, const uint8_t * b, size_t bStride, size_t width, size_t height, uint64_t * sum)
 //SimdLaplaceAbsSum(const uint8_t * src, size_t stride, size_t width, size_t height, uint64_t * sum)
+
+// ingroup row_statistic
+//SimdGetRowSums(const uint8_t * src, size_t stride, size_t width, size_t height, uint32_t * sums)
+//SimdGetAbsDyRowSums(const uint8_t * src, size_t stride, size_t width, size_t height, uint32_t * sums)
