4a0fe68878b32e5a11c5578dac4977a37aa1303a
frankw <fwessels@xs4all.nl>
2017-02-15 15:49:27 -0800

    Added HOG implementation (Histograms Of Gradients)

--- face-recognition.go
+++ face-recognition.go
@@ -1,5 +1,17 @@
 package main
 
 // ingroup face_recognition
-//SimdHogDirectionHistograms(const uint8_t * src, size_t stride, size_t width, size_t height, size_t cellX, size_t cellY, size_t quantization, float * histograms)
-//SimdInt16ToGray(const uint8_t * src, size_t width, size_t height, size_t srcStride, uint8_t * dst, size_t dstStride)
+
+// #cgo pkg-config: simd
+// #include "stdlib.h"
+// #include "Simd/SimdLib.h"
+// #cgo LDFLAGS: -lstdc++
+import "C"
+
+func HogDirectionHistograms(src View) float64 {
+	cellX, cellY := 0, 0
+	quantization := 0
+	histograms := C.float(0.0)
+	C.SimdHogDirectionHistograms((*C.uint8_t)(src.data), C.size_t(src.stride), C.size_t(src.width), C.size_t(src.height), C.size_t(cellX), C.size_t(cellY), C.size_t(quantization), &histograms)
+	return float64(histograms)
+}
