bc013ce5c0e6138cb7cbd636143b64c686085ac8
frankw <fwessels@xs4all.nl>
2017-02-14 20:54:42 -0800

    Initial benchmarks

---
+++ benchmarks_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"testing"
+	"github.com/lazywei/go-opencv/opencv"
+
+)
+
+const Resolution = 2048
+
+func SimdSetup() (View, View) {
+
+	a, b := View{}, View{}
+	a.Recreate(Resolution, Resolution, GRAY8)
+	b.Recreate(Resolution, Resolution, GRAY8)
+
+	return a, b
+}
+
+func BenchmarkSimdGaussian(b *testing.B) {
+
+	src, dst := SimdSetup()
+
+	for i := 0; i < b.N; i++ {
+		GaussianBlur3x3(src, dst)
+	}
+}
+
+func BenchmarkSimdBlur(b *testing.B) {
+
+	src, dst := SimdSetup()
+
+	for i := 0; i < b.N; i++ {
+		MeanFilter3x3(src, dst)
+	}
+}
+
+func BenchmarkSimdMedian3x3(b *testing.B) {
+
+	src, dst := SimdSetup()
+
+	for i := 0; i < b.N; i++ {
+		MedianFilterSquare3x3(src, dst)
+	}
+}
+
+func BenchmarkSimdMedian5x5(b *testing.B) {
+
+	src, dst := SimdSetup()
+
+	for i := 0; i < b.N; i++ {
+		MedianFilterSquare5x5(src, dst)
+	}
+}
+
+func OpenCVSetup() (*opencv.IplImage, *opencv.IplImage) {
+
+	a := opencv.CreateImage(Resolution, Resolution, 1, 1)
+	b := opencv.CreateImage(Resolution, Resolution, 1, 1)
+
+	return a, b
+}
+
+func BenchmarkOpenCVGaussian(b *testing.B) {
+
+	src, dst := OpenCVSetup()
+
+	for i := 0; i < b.N; i++ {
+		opencv.Smooth(src, dst, opencv.CV_GAUSSIAN, 3, 3, 0, 0)
+	}
+}
+
+func BenchmarkOpenCVBlur(b *testing.B) {
+
+	src, dst := OpenCVSetup()
+
+	for i := 0; i < b.N; i++ {
+		opencv.Smooth(src, dst, opencv.CV_BLUR, 3, 3, 0, 0)
+	}
+}
+
+func BenchmarkOpenCVMedian3x3(b *testing.B) {
+
+	src, dst := OpenCVSetup()
+
+	for i := 0; i < b.N; i++ {
+		opencv.Smooth(src, dst, opencv.CV_MEDIAN, 3, 3, 0, 0)
+	}
+}
+
+func BenchmarkOpenCVMedian5x5(b *testing.B) {
+
+	src, dst := OpenCVSetup()
+
+	for i := 0; i < b.N; i++ {
+		opencv.Smooth(src, dst, opencv.CV_MEDIAN, 5, 5, 0, 0)
+	}
+}
