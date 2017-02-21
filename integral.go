36fd3aaf6a06683c174efa04d2b0d4b0e7f3481a
frankw <fwessels@xs4all.nl>
2017-02-21 13:18:07 -0800

    Integral functions

--- integral.go
+++ integral.go
@@ -1,4 +1,12 @@
 package main
 
+// #cgo pkg-config: simd
+// #include "stdlib.h"
+// #include "Simd/SimdLib.h"
+// #cgo LDFLAGS: -lstdc++
+import "C"
+
 // ingroup integral
-//SimdIntegral(const uint8_t * src, size_t srcStride, size_t width, size_t height, uint8_t * sum, size_t sumStride, uint8_t * sqsum, size_t sqsumStride, uint8_t * tilted, size_t tiltedStride, SimdPixelFormatType sumFormat, SimdPixelFormatType sqsumFormat)
+func Integral(src, sum, sqsum, tilted View) {
+	C.SimdIntegral((*C.uint8_t)(src.data), C.size_t(src.stride), C.size_t(src.width), C.size_t(src.height), (*C.uint8_t)(sum.data), C.size_t(sum.stride), (*C.uint8_t)(sqsum.data), C.size_t(sqsum.stride), (*C.uint8_t)(tilted.data), C.size_t(tilted.stride), C.SimdPixelFormatType(sum.format), C.SimdPixelFormatType(sqsum.format))
+}
