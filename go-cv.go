215bb14e4be83dc8bb18abf11b00bcff12b1af1d
frankw <fwessels@xs4all.nl>
2017-02-24 12:17:35 -0800

    LoadImage into Simd

--- go-cv.go
+++ go-cv.go
@@ -6,10 +6,44 @@ package main
 // #cgo LDFLAGS: -lstdc++
 import "C"
 
-import "fmt"
+import (
+	"os"
+	"fmt"
+	"time"
+	"image"
+	"github.com/anthonynsimon/bild/imgio"
+)
 
 func Version() string {
 	return C.GoString(C.SimdVersion())
+}
+
+func LoadImage() {
+
+	reader, err := os.Open("/Users/frankw/c_apps/opencv-3.2.0/samples/data/lena.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer reader.Close()
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		panic(err)
+	}
+	img := AsRGBA(m)
+
+	src := View{}
+	src.Recreate(img.Bounds().Size().X, img.Bounds().Size().Y, BGRA32)
+	src.CopyFrom(img)
+
+	dst := View{}
+	dst.Recreate(src.width, src.height, src.format)
+
+	Copy(src, dst)
+
+	dst.CopyTo(img)
+	if err := imgio.Save("./filename", img, imgio.JPEG); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
